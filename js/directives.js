e6ea3144ff0af4d254a0e86330c5a617c95f2479
roman.orlovskyi <roman.orlovskyi@masterofcode.com>
2013-11-01 14:53:14 +0200

    some fixes in directives

--- js/directives.js
+++ js/directives.js
@@ -6,7 +6,7 @@ var appStyleSheet = null,
 
 app.directive('datePicker',function () {
 
-    return function (scope, element, attrs) {
+    return function (scope, element, attr) {
 
         scope.$watch('model.date', function (newValue, oldValue) {
 
@@ -100,16 +100,16 @@ app.directive('datePicker',function () {
                     block = '<ul id="moc-days-block">',//<a id="moc-days-block-toggle" data-show="">Show</a>
                     boot = '<div class="btn-group">' +
                         '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Reason <span class="caret"></span></button>' +
-                        '<ul class="dropdown-menu"><li ng-repeat="type in types" data-value="{{type.id}}"><a>{{type.title}}</a></li></ul></div>', /* +
-                 '<ul><li ng-repeat="type in types"><form name="moc_days_form_{{type.name}}" id="moc_days_form_{{type.name}}" class="moc_days_forms">' +
-                 '<div id="moc-colorpicker-{{type.name}}" class="moc-colorpicker-block" data-color="{{type.color}}" data-item="{{type.id}}">' +
-                 '<div edit-date-type="" name="{{type.name}}_color" ng-model="types.{{type.name}}.color" style="background-color: #{{type.color}};"></div>' +
+                        '<ul class="dropdown-menu"><li ng-repeat="type in types" data-value="{{type.id}}"><a>{{type.title}}</a></li></ul></div>' +
+                        '<ul><li ng-repeat="type in types"><form name="moc_days_form_{{type.name}}" id="moc_days_form_{{type.name}}" class="moc_days_forms">' +
+                        '<div id="moc-colorpicker-{{type.name}}" class="moc-colorpicker-block" data-color="{{type.color}}" data-item="{{type.id}}">', /*+
+                 '<div edit-date-type="" name="{{type.name}}_color" ng-model="type.color" style="background-color: #{{type.color}};"></div>' +// ng-model="types.{{type.name}}.color"
                  '</div>' +
-                 '<span class="" ng-show="!types.{{type.name}}.edit"> - </span>' +
-                 '<input type="text" ng-show="types.{{type.name}}.edit" data-title="{{type.title}}" ng-model="types.{{type.name}}.title" name="{{type.name}}_title" class="moc-edit-title form-control" required>' +
-                 '<button type="button" class="btn btn-primary" edit-date-title="{{type.name}}">Edit</button>' +
-                 '<button type="button" class="btn btn-success" update-date-type="{{type.name}}" ng-disabled="moc_days_form_{{type.name}}.$pristine">Save</button>' +
-                 '<button type="button" class="btn btn-warning" cancel-date-type="{{type.name}}" ng-model="types.{{type.name}}" ng-disabled="moc_days_form_{{type.name}}.$pristine">Cancel</button></form></li></ul>',*/
+                 '<span class="" ng-show="!type.edit"> - {{type.title}} </span>' +
+                 '<input type="text" ng-show="type.edit" data-title="{{type.title}}" ng-model="type.title" name="{{type.name}}_title" class="moc-edit-title form-control" required>' +//ng-model="types.{{type.name}}.title"
+                 '<button type="button" class="btn btn-primary" edit-date-title="{{type.name}}" ng-model="type">Edit</button>' +
+                 '<button type="button" class="btn btn-success" update-date-type="{{type.name}}" ng-model="type" ng-disabled="moc_days_form_{{type.name}}.$pristine">Save</button>' +
+                 '<button type="button" class="btn btn-warning" cancel-date-type="{{type.name}}" ng-model="type" ng-disabled="moc_days_form_{{type.name}}.$pristine">Cancel</button></form></li></ul>',//ng-model="types.{{type.name}}"*/
                     itemName = '';
 
                 scope.types = {};
@@ -118,21 +118,29 @@ app.directive('datePicker',function () {
 
                     itemName = 'item' + value.id;
 
-                    scope.types[itemName] = {id: value.id, title: value.title, color: value.color, name: itemName, edit: false};
+                    scope.types[itemName] = {
+                        id: value.id,
+                        title: value.title,
+                        color: value.color,
+                        name: itemName,
+                        edit: false,
+                        active: false
+                    };
 
                     list += '<option value="' + value.id + '">{{types.' + itemName + '.title}}</option>';
                     style += appClassTemplate(itemName, value.color, false);
                     block += '<li><form name="moc_days_form_' + itemName + '" id="moc_days_form_' + itemName + '" class="moc_days_forms">' +
                         '<div class="moc-type-item">' +
+                        '<span class=".glyphicon .glyphicon-ok"></span>' +
                         '<div id="moc-colorpicker-' + itemName + '" class="moc-colorpicker-block" data-color="' + value.color + '" data-item="' + value.id + '">' +
                         '<div edit-date-type="" name="' + itemName + '_color" ng-model="types.' + itemName + '.color" style="background-color: #' + value.color + ';"></div>' +
                         '</div>' +
                         '<span class="" ng-show="!types.' + itemName + '.edit"> - {{types.' + itemName + '.title}}</span></div>' +
                         '<div class="col-md-2" ng-show="types.' + itemName + '.edit"><input type="text" data-title="' + value.title + '" ng-model="types.' + itemName + '.title" name="' + itemName + '_title" class="moc-edit-title form-control" required></div>' +
                         '<div class="moc-types-buttons">' +
-                        '<button type="button" class="btn btn-primary" edit-date-title="' + itemName + '">Edit</button>' +
-                        '<button type="button" class="btn btn-success" update-date-type="' + itemName + '" ng-disabled="moc_days_form_' + itemName + '.$pristine">Save</button>' +
-                        '<button type="button" class="btn btn-warning" cancel-date-type="' + itemName + '" ng-model="types.' + itemName + '" ng-disabled="moc_days_form_' + itemName + '.$pristine">Cancel</button>' +
+                        '<button type="button" class="btn btn-primary" edit-date-title="" ng-model="types.' + itemName + '">Edit</button>' +
+                        '<button type="button" class="btn btn-success" update-date-type="" ng-model="types.' + itemName + '" ng-disabled="moc_days_form_' + itemName + '.$pristine">Save</button>' +
+                        '<button type="button" class="btn btn-warning" cancel-date-type="" ng-model="types.' + itemName + '" ng-disabled="moc_days_form_' + itemName + '.$pristine">Cancel</button>' +
                         '</div></form></li>';
                 });
 
@@ -228,55 +236,73 @@ app.directive('datePicker',function () {
             }
         };
     }).directive('cancelDateType',function (DIRTY_CLASS, PRISTINE_CLASS) {
-        return function (scope, element, attr) {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attr, ngModel) {
 
-            (function ($) {
+                (function ($) {
 
-                $(element).on('click', function () {
+                    $(element).on('click', function () {
 
-                    var parent = $(element).closest('.moc_days_forms').removeClass(DIRTY_CLASS).addClass(PRISTINE_CLASS),
-                        picker = parent.find('div.moc-colorpicker-block'),
-                        color = picker.data('color'),
-                        editTitle = $('input.moc-edit-title', parent).removeClass(DIRTY_CLASS).addClass(PRISTINE_CLASS);
+                        var parent = $(element).closest('.moc_days_forms').removeClass(DIRTY_CLASS)
+                                .addClass(PRISTINE_CLASS),
+                            picker = parent.find('div.moc-colorpicker-block'),
+                            color = picker.data('color'),
+                            editTitle = $('input.moc-edit-title', parent).removeClass(DIRTY_CLASS)
+                                .addClass(PRISTINE_CLASS);
 
-                    picker.ColorPickerSetColor(color).find('div').css('background-color', '#' + color);
+                        picker.ColorPickerSetColor(color).find('div').css('background-color', '#' + color);
 
-                    scope.resetTypeForm(attr.cancelDateType, editTitle.data('title'), false);
+                        scope.resetTypeForm(ngModel.$modelValue.name, editTitle.data('title'), false);
 
-                });
+                    });
 
-            })(jQuery);
+                })(jQuery);
+            }
         };
     }).directive('editDateTitle',function () {
-        return function (scope, element, attr) {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attr, ngModel) {
 
-            element.bind('click', function () {
+                element.bind('click', function () {
 
-                scope.editDateTitle(attr.editDateTitle);
-
-            });
-        };
-    }).directive('updateDateType',function (DIRTY_CLASS, PRISTINE_CLASS) {
-        return function (scope, element, attr) {
-
-            (function ($) {
-
-                $(element).on('click', function () {
-
-                    var parent = $(element).closest('.moc_days_forms').removeClass(DIRTY_CLASS).addClass(PRISTINE_CLASS);
-
-                    parent.find('div.moc-colorpicker-block').data('color', scope.types[attr.updateDateType].color);
-                    $('input.moc-edit-title', parent).removeClass(DIRTY_CLASS).addClass(PRISTINE_CLASS).data('title', scope.types[attr.updateDateType].title);
-
-                    scope.saveDateType(attr.updateDateType);
-
-                    if (scope.types[attr.updateDateType].edit) {
-                        scope.editDateTitle(attr.updateDateType);
-                    }
+                    scope.editDateTitle(ngModel.$modelValue.name);
 
                 });
+            }
+        };
+    }).directive('updateDateType',function (DIRTY_CLASS, PRISTINE_CLASS) {
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attr, ngModel) {
 
-            })(jQuery);
+                (function ($) {
+
+                    $(element).on('click', function () {
+
+                        var parent = $(element).closest('.moc_days_forms').removeClass(DIRTY_CLASS)
+                            .addClass(PRISTINE_CLASS);
+
+                        parent.find('div.moc-colorpicker-block')
+                            .data('color', scope.types[ngModel.$modelValue.name].color);
+
+                        $('input.moc-edit-title', parent).removeClass(DIRTY_CLASS).addClass(PRISTINE_CLASS)
+                            .data('title', scope.types[ngModel.$modelValue.name].title);
+
+                        scope.saveDateType(ngModel.$modelValue.name);
+
+                        if (scope.types[ngModel.$modelValue.name].edit) {
+                            scope.editDateTitle(ngModel.$modelValue.name);
+                        }
+
+                    });
+
+                })(jQuery);
+            }
         };
     }).directive('resultMessage', function () {
         return function (scope, element, attr) {
